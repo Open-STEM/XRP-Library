c795257a2692a887f6fb99da9ac1936dfedbf1f3
zeke2856 <46726503+zeke2856@users.noreply.github.com>
2022-05-24 16:59:48 -0400

    Create Encoder.py

---
+++ Encoder.py
@@ -0,0 +1,28 @@
+
+# Write your code here :-)
+import rotaryio
+
+class Encoder():
+
+    def __init__(self, a, b, ticks, flip=False):
+        self.ticksPerRev = ticks
+        self.reverse = flip
+        self.encoder = rotaryio.IncrementalEncoder(a, b)
+        print("initialized")
+
+    def getPos(self, inTicks=False, roundToo=3):
+        #print("getPos")
+        r = self.encoder.position
+        if self.reverse:
+            r *= -1
+        if not inTicks:
+            r = r / self.ticksPerRev
+        return round(r, roundToo)
+
+    def setPos(self, pos=0):
+        """
+            pos    float   number of rotations to set encoder to
+        """
+        self.encoder.position = round(pos * self.ticksPerRev)
+
+
