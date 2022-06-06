bb0a24011e63dcfaf94e34c47d380982dc6076ce
zeke2856 <46726503+zeke2856@users.noreply.github.com>
2022-06-06 17:49:07 -0400

    Messing Around With Code

--- code.py
+++ code.py
@@ -4,36 +4,105 @@ import pwmio
 import rotaryio
 from adafruit_motor import motor
 import Encoder as enc
+from simple_pid import PID
+import encodedMotor as em
 
+# Motor Right
+encR = enc.encoder(a=board.GP26, b=board.GP27, ticks=144)
+mR = em.EncodedMotor(encR, board.GP8, board.GP9, "Motor Right", False)
 
-# Motor 1 is Right
+# Motor Left
+encL = enc.encoder(a=board.GP16, b=board.GP17, ticks=144)
+mL = em.EncodedMotor(encL, board.GP10, board.GP11, "Motor Left", True)
 
-# Motor 1
-PWM_M1A = board.GP8
-PWM_M1B = board.GP9
+mL.setEffort(1)
+mR.setEffort(1)
 
-# Motor 2 is Left
-PWM_M2A = board.GP10
-PWM_M2B = board.GP11
+turns = 4
+straight = 5
+rotationsToTurn = .78
+while turns > 0:
+    print(turns)
+    turns -= 1
 
-# DC motor setup
-M1A = pwmio.PWMOut(PWM_M1A, frequency=10000)
-M1B = pwmio.PWMOut(PWM_M1B, frequency=10000)
-M2A = pwmio.PWMOut(PWM_M2A, frequency=10000)
-M2B = pwmio.PWMOut(PWM_M2B, frequency=10000)
+    ## Straight
+    while ((mL.getPos() + mR.getPos())/2) < straight:
+        if mL.getPos() > mR.getPos():
+            mL.setEffort(0)
+            mR.setEffort(1)
+        else:
+            mL.setEffort(1)
+            mR.setEffort(0)
+    mL.setPos()
+    mR.setPos()
 
-motor1 = motor.DCMotor(M1A, M1B)
-motor2 = motor.DCMotor(M2A, M2B)
+    ## Turn
+    while ((- mL.getPos() + mR.getPos())/2) < rotationsToTurn:
+        if -mL.getPos() > mR.getPos():
+            mL.setEffort(0)
+            mR.setEffort(1)
+        else:
+            mL.setEffort(-1)
+            mR.setEffort(0)
+    mL.setPos()
+    mR.setPos()
 
-enc1 = enc.Encoder(a=board.GP26, b=board.GP27, ticks=144)
-enc2 = enc.Encoder(a=board.GP16, b=board.GP17, ticks=144)
+while False:
+    print("Enc 1 " + str(mL.getPos()) + " Enc 2 " + str(mR.getPos()))
+    time.sleep(.1)
 
-motor1.throttle = 1
-motor2.throttle = 1
+speed = .3
+startTime = time.monotonic() - .001
+while False:
+    timeDelta = (time.monotonic() - startTime)
+    #print("StartTime " + str(startTime))
 
-while enc2.getPos() < 10000:
+    # print("mL")
+    # print(((mL.getPos() / timeDelta),))
+    if ((mL.getPos() / timeDelta) < speed):
+        mL.setEffort(1)
+    else:
+        mL.setEffort(0)
+
+    # print("mR")
+    # print(((mR.getPos() / timeDelta),))
+    if ((mR.getPos() / timeDelta) < speed):
+        mR.setEffort(1)
+    else:
+        mR.setEffort(0)
+
+    # print("Enc L " + str(mL.getPos()) + " Enc R " + str(mR.getPos()))
+    # print("TimeDelta: " + str(timeDelta))
+    time.sleep(.001)
+
+speedArr = (0,0,0,0,0,0,0,0)
+while True:
+    prevPos = mL.getPos()
+    prevTime = time.monotonic()
+    time.sleep(.03)
+    currSpeed = (mL.getPos() - prevPos)/(time.monotonic() - prevTime)
+
+    speedArr.pop(0)
+    speedArr.append(currSpeed)
+    average = sum(speedArr) / len(speedArr)
+
+    #print(str(round(average, 3)) + "  " + str(len(speedArr)) + "  Current Speed   " + str(currSpeed))
+
+    print((average,sp, motor2.throttle))
+    #print((motor2.throttle,))
+
+    # Compute new output from the PID according to the systems current value
+    pidOut = pid(average)
+    mL.throttle = max(-1, min(pidOut, 1))
+
+    #print("  Current Speed   " + str(currSpeed) + "  Current Throttle:    " + str(motor2.throttle))
+
+while False:
     time.sleep(.01)
-    print(enc2.getPos())
+    error = enc1.getPos() - target
+
+    motor1.throttle = max(min(3, 1), -1)
+    print(enc1.getPos())
 
 print("reset encoder")
 enc2.setPos()
