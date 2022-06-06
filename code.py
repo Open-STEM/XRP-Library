7033ea842dc32093136649263cff0c1da4e45ab8
zeke2856 <46726503+zeke2856@users.noreply.github.com>
2022-06-06 17:50:07 -0400

    Update code.py

--- code.py
+++ code.py
@@ -4,40 +4,45 @@ import pwmio
 import rotaryio
 from adafruit_motor import motor
 import Encoder as enc
+from simple_pid import PID
+import encodedMotor as em
 
+# Motor Right
+encR = enc.encoder(a=board.GP26, b=board.GP27, ticks=144)
+mR = em.EncodedMotor(encR, board.GP8, board.GP9, "Motor Right", False)
 
-# Motor 1 is Right
+# Motor Left
+encL = enc.encoder(a=board.GP16, b=board.GP17, ticks=144)
+mL = em.EncodedMotor(encL, board.GP10, board.GP11, "Motor Left", True)
 
-# Motor 1
-PWM_M1A = board.GP8
-PWM_M1B = board.GP9
+mL.setEffort(1)
+mR.setEffort(1)
 
-# Motor 2 is Left
-PWM_M2A = board.GP10
-PWM_M2B = board.GP11
+turns = 4
+straight = 5
+rotationsToTurn = .78
+while turns > 0:
+    print(turns)
+    turns -= 1
 
-# DC motor setup
-M1A = pwmio.PWMOut(PWM_M1A, frequency=10000)
-M1B = pwmio.PWMOut(PWM_M1B, frequency=10000)
-M2A = pwmio.PWMOut(PWM_M2A, frequency=10000)
-M2B = pwmio.PWMOut(PWM_M2B, frequency=10000)
+    ## Straight
+    while ((mL.getPos() + mR.getPos())/2) < straight:
+        if mL.getPos() > mR.getPos():
+            mL.setEffort(0)
+            mR.setEffort(1)
+        else:
+            mL.setEffort(1)
+            mR.setEffort(0)
+    mL.setPos()
+    mR.setPos()
 
-motor1 = motor.DCMotor(M1A, M1B)
-motor2 = motor.DCMotor(M2A, M2B)
-
-enc1 = enc.Encoder(a=board.GP26, b=board.GP27, ticks=144)
-enc2 = enc.Encoder(a=board.GP16, b=board.GP17, ticks=144)
-
-motor1.throttle = 1
-motor2.throttle = 1
-
-while enc2.getPos() < 10000:
-    time.sleep(.01)
-    print(enc2.getPos())
-
-print("reset encoder")
-enc2.setPos()
-
-while enc2.getPos() < 10000:
-    time.sleep(.01)
-    print(enc2.getPos())
+    ## Turn
+    while ((- mL.getPos() + mR.getPos())/2) < rotationsToTurn:
+        if -mL.getPos() > mR.getPos():
+            mL.setEffort(0)
+            mR.setEffort(1)
+        else:
+            mL.setEffort(-1)
+            mR.setEffort(0)
+    mL.setPos()
+    mR.setPos()
