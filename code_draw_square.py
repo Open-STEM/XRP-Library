7de87eb72967e85d43c935e1f5070a8f360cc631
bradamiller <brad@bradhouse.com>
2022-07-06 10:28:01 -0400

    Update code_draw_square.py
    Update the actual draw square file

--- code_draw_square.py
+++ code_draw_square.py
@@ -1,47 +1,8 @@
-import time
-import board
-import encoder as enc
-import encoded_motor as em
-import drive as drv
-
-"""
-    File meant to help student calibrate io.
-
-    By the end of completing this file the student's robot should
-    have both motors spinning forward and the encoders counting up.
-"""
-
-# Encoders
-encL = enc.encoder(a=board.GP4, b=board.GP5, ticksPerRev=144, doFlip=False)
-encR = enc.encoder(a=board.GP2, b=board.GP3, ticksPerRev=144, doFlip=True)
-
-# Motors
-mL = em.encoded_motor(encL, board.GP8, board.GP9, "Motor Left", doFlip=True)
-mR = em.encoded_motor(encR, board.GP10, board.GP11, "Motor Right", doFlip=False)
+from WPILib import *
 
 # Drive Base
-driveBase = drv.drive(mL, mR)
+driveBase = drv.drive()
 
-turns = 4
-straight = 4
-rotationsToTurn = 0.66
-
-while turns > 0:
-    print(turns)
-    turns -= 1
-
-    # Straight
-    while ((mL.getPos() + mR.getPos()) / 2) < straight:
-        if mL.getPos() > mR.getPos():
-            driveBase.setEffort(0, 1)
-        else:
-            driveBase.setEffort(1, 0)
-    driveBase.setPos()
-
-    # Turn
-    while ((-mL.getPos() + mR.getPos()) / 2) < rotationsToTurn:
-        if -mL.getPos() > mR.getPos():
-            driveBase.setEffort(0, 1)
-        else:
-            driveBase.setEffort(-1, 0)
-    driveBase.setPos()
+for side in range(4):
+    driveBase.straight(100)
+    driveBase.turn(90)
