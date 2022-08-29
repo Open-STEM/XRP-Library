308303999106b533ba81404efc13866970c1a715
Kevin Siegall <digimonlord2@gmail.com>
2022-08-29 16:41:23 -0400

    Replaced all sample code

---
+++ SampleCode/sample_sensor_access.py
@@ -0,0 +1,19 @@
+from WPILib.WPILib import *
+
+# Polling data from the ultrasonic sensor
+def ultrasonicTest():
+    while False:
+        try:
+            print((sonar.getDistance()))
+        except RuntimeError:
+            print("Retrying!")
+            pass
+        time.sleep(0.1)
+
+# Follows a line using the line followers
+def lineTrack():
+    baseEffort = 0.6
+    KP = 0.02
+    while True:
+        error = reflectance.getLeftReflectance() - reflectance.getRightReflectance()
+        drivetrain.setEffort(baseEffort + error * KP, baseEffort -  error * KP)
