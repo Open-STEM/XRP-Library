3e54f0496b084365fe987627d7fea51a41ed22d6
Kevin Siegall <digimonlord2@gmail.com>
2022-09-05 00:54:24 -0400

    added docs to reflectance

--- WPILib/_reflectance.py
+++ WPILib/_reflectance.py
@@ -6,7 +6,17 @@ class Reflectance:
         self._rightReflectance = _grove_ultrasonic.GroveUltrasonicRanger(rightPin)
     
     def get_left_reflectance(self):
+        """
+        Retrieves the reflectance value from the left line follower
+        :return: The reflectance value
+        :rtype: float
+        """
         return self._leftReflectance.get_distance()
     
     def get_right_reflectance(self):
+        """
+        Retrieves the reflectance value from the right line follower
+        :return: The reflectance value
+        :rtype: float
+        """
         return self._rightReflectance.get_distance()
